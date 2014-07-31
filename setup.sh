d4f3fcc2e317e6803fb295eadf7551a6376ab8bf
Piyush Harsh <piyush.harsh@zhaw.ch>
2014-07-31 10:44:59 +0200

    latest commit from Tea

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /usr/local/bin/python
+
+sudo apt-get update
+
+sudo apt-get install -y python-pip
+
+pip install sympy
+
+pip install httplib2
+
+sudo apt-get install python2.7-mysqldb
+
+sudo apt-get install mysql-server
+
+pip install mysql-connector-python --allow-external mysql-connector-python
